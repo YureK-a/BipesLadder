c3d4c68fab8d1c812a8aced69b02c33ac4f1c155
mariusxaxae <melonston@gmail.com>
2021-06-28 23:32:50 -0300

    React konva

--- Site/Testes React/saida.js
+++
@@ -1,5 +0,0 @@
-function Palco() {
-  return /*#__PURE__*/React.createElement("div", null, " Ola Mundozada ");
-}
-
-ReactDOM.render( /*#__PURE__*/React.createElement(Palco, null), document.getElementById('react'));
---
+++ saida.js
@@ -0,0 +1,21 @@
+import Konva from 'konva';
+import  ReactDOM  from 'react-dom';
+import React from 'react';
+import { Stage, Layer, Rect } from 'react-konva';
+
+function Palco() {
+  return /*#__PURE__*/React.createElement(Stage, {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }, /*#__PURE__*/React.createElement(Layer, null, /*#__PURE__*/React.createElement(Rect, {
+    x: -100,
+    y: -150,
+    width: 350,
+    height: 8000,
+    fill: "white",
+    stroke: "#212529",
+    strokeWidth: 7
+  })));
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Palco, null), document.getElementById('react'));
