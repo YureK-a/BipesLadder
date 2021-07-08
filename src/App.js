813bef017e81127510499be58d2e8a9c7147d1f1
mariusxaxae <melonston@gmail.com>
2021-07-07 21:17:25 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
+
+const fotosTestes = [
+  {
+    id: 'teste',
+    name: 'Imagem 1',
+    thumb: '/images/paratestes.png'
+  },
+  {
+    id: 'teste2',
+    name: 'Imagem 2',
+    thumb: '/images/paratestes.png'
+  },
+  {
+    id: 'teste3',
+    name: 'Imagem 3',
+    thumb: '/images/paratestes.png'
+  },
+  {
+    id: 'teste4',
+    name: 'Imagem 4',
+    thumb: '/images/paratestes.png'
+  }
+]
+
 function App() {
+
+  const [fotos, atualizarFotos] = useState(fotosTestes);
+  
+  function handleOnDragEnd(result) {
+        if (!result.destination) return;
+  
+      const items = Array.from(fotos);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+  
+      atualizarFotos(items);
+  }
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Teste de listas</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+            <div id="divDaLista">
+              <Droppable droppableId="fotos">
+                {(provided) => (
+                  <ul className="listaDeFotos" {...provided.droppableProps} ref={provided.innerRef}>
+                    {fotos.map(({id, name, thumb}, index) => {
+                      return (
+                        <Draggable key={id} draggableId={id} index={index}>
+                          {(provided) => (
+                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <div className="fotos-thumb">
+                                <img src={thumb} alt={` ${name} Thumb`} />
+                              </div>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </div>
+
+        </DragDropContext>
       </header>
     </div>
   );
