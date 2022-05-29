074a6220c4433c80a4bb5469b2024acfbab8096b
YureK-a <78214862+YureK-a@users.noreply.github.com>
2022-05-29 14:56:56 -0300

    Update Column.jsx

--- bipes_ladder_ui/src/Column.jsx
+++ bipes_ladder_ui/src/Column.jsx
@@ -3,43 +3,9 @@ import { useDrag } from "react-dnd";
 import { COLUMN } from "./constants";
 import DropZone from "./DropZone";
 import Component from "./Component";
-
-const style_vertical_div = {
-  backgroundColor: "#dee0de",
-  width: "3px",
-  position: "absolute",
-  height: "110px",
-  marginTop: "68px",
-  marginLeft: "-3px",
-};
-
-const checkIfwasSelected = (path, row) => parallelLines[row].includes(path);
-
-const changeLineColor = (color, path) => document
-  .getElementById("parallel_line-" + path)
-  .style.backgroundColor = color
-
-const showLine = (path, rowPath) => {
-  const row = rowPath.split("-")[0];
-  const wasSelected = checkIfwasSelected(path, row)
-
-  if (wasSelected) {
-    changeLineColor('#dee0de', path)
-    parallelLines[row]
-      .splice(parallelLines[row]
-      .indexOf(path));
-
-      return
-  }
-
-  changeLineColor('Black', path)
-  parallelLines[row].push(path);
-};
+import ParallelLines from "./ParallelLines"
 
 const style = {};
-const parallelLines = new Array(5)
-  .fill("")
-  .map(() => new Array(15).fill(""));
 
 let rowPath;
 
@@ -104,6 +70,7 @@ const Column = ({
           </React.Fragment>
         );
       })}
+      
       <DropZone
         data={{
           path: `${path}-${data.children.length}`,
@@ -114,14 +81,13 @@ const Column = ({
         isLast
       />
 
-      <div
-        id={"parallel_line-" + path}
-        style={style_vertical_div}
-        onClick={() => showLine(path, rowPath)}
-      ></div>
+      <ParallelLines
+        path={path}
+        rowPath={rowPath}
+      />
+
     </div>
   );
 };
 
 export default Column;
-export { parallelLines };
